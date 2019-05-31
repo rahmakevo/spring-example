baf1b3d51d54ac576d512ed8b6080774f8f9d9a6
rahmakevo <siundu344@gmail.com>
2019-05-31 15:12:56 +0300

    working on auth controller

---
+++ src/main/java/com/microservice/example/controllers/JwtAuthenticationController.java
@@ -0,0 +1,42 @@
+package com.microservice.example.controllers;
+
+import com.microservice.example.config.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class JwtAuthenticationController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    JwtUserDetailsService userDetailsService;
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest request) throws Exception {
+        authenticate(request.getUsername(), request.getPassword());
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("BAD CREDENTIALS", e);
+        }
+    }
+}
