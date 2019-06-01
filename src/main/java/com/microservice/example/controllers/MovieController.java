b248fb02dcc8c6317a4d3dc9e44334e2b6a15ccd
rahmakevo <siundu344@gmail.com>
2019-06-01 22:46:42 +0300

    refactor code'

--- src/main/java/com/microservice/example/controllers/MovieController.java
+++ src/main/java/com/microservice/example/controllers/MovieController.java
@@ -4,6 +4,7 @@ import com.microservice.example.model.MovieModel;
 import com.microservice.example.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +13,10 @@ public class MovieController {
     @Autowired
     MovieRepository repository;
 
-    @GetMapping("/save")
+    @PostMapping("/save")
     public MovieModel saveAll(@RequestBody MovieModel model) {
         return repository.save(model);
     }
 
-    
+
 }
