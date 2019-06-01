f8eb7b87b79636fe83a19512a833f1a71d6a8d96
rahmakevo <siundu344@gmail.com>
2019-06-01 15:14:35 +0300

    refactor code'

--- src/main/java/com/microservice/example/controllers/HelloWorldController.java
+++ src/main/java/com/microservice/example/controllers/HelloWorldController.java
@@ -1,13 +1,14 @@
 package com.microservice.example.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class HelloWorldController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping({ "/hello" })
     public String firstPage() {
         return "Hello World!";
     }
--- src/main/java/com/microservice/example/security/WebSecurityConfig.java
+++ src/main/java/com/microservice/example/security/WebSecurityConfig.java
@@ -49,19 +49,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-// We don't need CSRF for this example
+        // We don't need CSRF for this example
         httpSecurity.csrf().disable()
-// dont authenticate this particular request
+                // dont authenticate this particular request
                 .authorizeRequests()
-                .antMatchers("/authenticate").permitAll()
-                .antMatchers("/hello").authenticated().
-// all other requests need to be authenticated
+                .antMatchers("/authenticate").permitAll().
+                // all other requests need to be authenticated
         anyRequest().authenticated().and().
-// make sure we use stateless session; session won't be used to
-// store user's state.
+                // make sure we use stateless session; session won't be used to
+                // store user's state.
         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-// Add a filter to validate the tokens with every request
+        // Add a filter to validate the tokens with every request
         httpSecurity.addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
