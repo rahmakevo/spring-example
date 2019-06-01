3bd5403bc754a09ed605759ef900976766119ba3
rahmakevo <siundu344@gmail.com>
2019-06-01 15:41:04 +0300

    working on user repository

---
+++ src/main/java/com/microservice/example/repository/UserDao.java
@@ -0,0 +1,9 @@
+package com.microservice.example.repository;
+
+import com.microservice.example.model.DAOUser;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<DAOUser, Integer> {
+}
