d912ae11e861aff17993acdc7a1e79f0d57f9e8b
rahmakevo <siundu344@gmail.com>
2019-06-01 19:34:48 +0300

    refactor code

--- src/main/java/com/microservice/example/service/JwtUserDetailsService.java
+++ src/main/java/com/microservice/example/service/JwtUserDetailsService.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,7 +19,7 @@ public class JwtUserDetailsService implements UserDetailsService {
     UserRepository userRepository;
 
     @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private PasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
