b6882513cbfafd6f670791f9ea7edc4a47ec46bb
rahmakevo <siundu344@gmail.com>
2019-06-01 15:21:23 +0300

    refactor code

--- src/main/java/com/microservice/example/controllers/HelloWorldController.java
+++ src/main/java/com/microservice/example/controllers/HelloWorldController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin()
 public class HelloWorldController {
 
     @RequestMapping({ "/hello" })
-    public String firstPage() {
-        return "Hello World!";
+    public String hello() {
+        return "Hello World";
     }
 
 }
