222c9a86c4beffc34ac3b6f8c3cead84f2fdb0fc
rahmakevo <siundu344@gmail.com>
2019-06-01 14:58:18 +0300

    refactor code

--- src/main/java/com/microservice/example/controllers/HelloWorldController.java
+++ src/main/java/com/microservice/example/controllers/HelloWorldController.java
@@ -1,12 +1,13 @@
 package com.microservice.example.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping({"/hello"})
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String firstPage() {
         return "Hello World!";
     }
--- src/main/java/com/microservice/example/security/WebSecurityConfig.java
+++ src/main/java/com/microservice/example/security/WebSecurityConfig.java
@@ -53,8 +53,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity.csrf().disable()
 // dont authenticate this particular request
                 .authorizeRequests()
-                .antMatchers("/hello").authenticated()
-                .antMatchers("/authenticate").permitAll().
+                .antMatchers("/authenticate").permitAll()
+                .antMatchers("/hello").authenticated().
 // all other requests need to be authenticated
         anyRequest().authenticated().and().
 // make sure we use stateless session; session won't be used to
