a57db594342afb955cd9e7593a7e39f7198770c6
rahmakevo <siundu344@gmail.com>
2019-06-01 15:28:42 +0300

    refactor code

--- src/main/java/com/microservice/example/security/WebSecurityConfig.java
+++ src/main/java/com/microservice/example/security/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
                 .authorizeRequests()
-                .antMatchers("/authenticate", "/hello").permitAll().
+                .antMatchers("/authenticate").permitAll().
                 // all other requests need to be authenticated
         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
