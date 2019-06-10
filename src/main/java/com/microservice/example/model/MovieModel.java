f766806d84a061c95dad5b0b634e4d3308b45ebf
rahmakevo <siundu344@gmail.com>
2019-06-10 14:51:01 +0300

    refactor code'

--- src/main/java/com/microservice/example/model/MovieModel.java
+++ src/main/java/com/microservice/example/model/MovieModel.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.NotNull;
 public class MovieModel {
     @NotNull
     @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
 
     private String title;
--- src/main/java/com/microservice/example/model/UserModel.java
+++ src/main/java/com/microservice/example/model/UserModel.java
@@ -3,14 +3,13 @@ package com.microservice.example.model;
 import lombok.Data;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
 @Table(name = "user")
 public class UserModel {
     @Id
-    @NotNull
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
 
     private String username;
